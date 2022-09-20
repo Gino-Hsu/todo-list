40ed5a2fc3a8179877860bdad19a6d4f4b14810f
GinoHsu <ginohsu16@gmail.com>
2022-09-20 22:47:28 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override') // 引用 metnod-override
 
-const Todo = require('./models/todo') // 載入 Todo model
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 建立一個名為 hbs 的樣板引擎，並傳入 exphbs 與相關參數
 app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: '.hbs' }))
@@ -18,17 +16,6 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // 將 request 導入路由器
 app.use(routes)
