6884c90c3d2d59af73eabad2975cd1e627fe73c7
GinoHsu <ginohsu16@gmail.com>
2022-09-26 20:17:47 +0800

    feat: add logout feature

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
 const session = require('express-session')
+const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override') // 引用 metnod-override
 
@@ -19,6 +19,9 @@ const usePassport = require('./config/passport')
 app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
@@ -32,8 +35,6 @@ app.use(methodOverride('_method'))
 // 將 request 導入路由器
 app.use(routes)
 
-// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
-usePassport(app)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
