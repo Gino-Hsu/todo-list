08060b11ae87b72d31eed9ded08d67913b7af52b
GinoHsu <ginohsu16@gmail.com>
2022-09-26 19:31:54 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override') // 引用 metnod-override
 
+
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -12,6 +13,8 @@ const app = express()
 // 否則為本地環境，使用 3000 
 const PORT = process.env.PORT || 3000
 
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
 // 建立一個名為 hbs 的樣板引擎，並傳入 exphbs 與相關參數
 app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -28,6 +31,9 @@ app.use(methodOverride('_method'))
 
 // 將 request 導入路由器
 app.use(routes)
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
