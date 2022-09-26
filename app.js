d4f8981fd15b23116749090b737a88578eace5de
GinoHsu <ginohsu16@gmail.com>
2022-09-26 17:48:08 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override') // 引用 metnod-override
 
@@ -15,6 +16,11 @@ const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
