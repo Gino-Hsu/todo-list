7170833f5ad6c960278cd26728ea2be763e16c21
GinoHsu <ginohsu16@gmail.com>
2022-09-26 15:23:45 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@
 const express = require('express')
 const router = express.Router()
 
-// 引入 home 模組程式碼
+// 引入模組程式碼
 const home = require('./modules/home')
 const todos = require('./modules/todos')
-// 將網址結構符合 / 字串的 request 導向 home 模組 
+const users = require('./modules/users')
+// 將網址結構符合字串的 request 導向模組 
 router.use('/', home)
-
 router.use('/todos', todos)
+router.use('/users', users)
+
 
 // 匯出路由器
 module.exports = router
